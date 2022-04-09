04f144a6f5195f2b6fb721c2335aecdad7258365
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-04-09 11:27:05 -0500

    subida backup secure

---
+++ demo/precache-manifest.6d5a80fef3fadafba123f89b5c07cafd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe3b49dfc61e287834fb6a09ca2cc0d",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "6aefe8fa56e31d51207e",
+    "url": "/demo/static/js/2.f571b379.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/demo/static/js/2.f571b379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1b69185151fec5db7b",
+    "url": "/demo/static/js/main.e95e9b09.chunk.js"
+  },
+  {
+    "revision": "8782decb24c07f68a240",
+    "url": "/demo/static/js/runtime-main.185d08bc.js"
+  }
+]);
---
+++ precache-manifest.80a3fd10dcc08cf1f7254d3fb666ea7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08938ff42016585497dd912130861f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "504acc7ab6ba5df8e966",
+    "url": "/static/js/2.687399a2.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/static/js/2.687399a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68cb92344ff9c37175aa",
+    "url": "/static/js/main.95c92008.chunk.js"
+  },
+  {
+    "revision": "606bfeb4c3a8741336af",
+    "url": "/static/js/runtime-main.d20aca80.js"
+  }
+]);
